5432a7c1e479e64e47e890535789864171612890
EllieHan93 <hjhhahahoho@naver.com>
2020-07-13 21:31:42 +0900

    컴포넌트 만들기

--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -1,75 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function Food({ name, rating }) {
-    // console.{log}(props)
-    // return <h1> I love Potato</h1>;
-    return (
-        <div>
-            <h2> I Like {name} </h2>
-            <h4> {rating}/5.0 </h4>
-        </div>
-    );
+//클래스 형 컴포넌트가 되려면, 리액트가 제공하는 컴포넌트 클래스를 반드시 상속받아야 한다. 
+class App extends React.Component{
+    //클래스형 컴포넌트에서는JSX 반환하기 위해서 render 함수를 사용한다.
+    render(){
+        return <h1>I'm a class Component</h1>
+    }
 }
-
-const foodLike = [
-    {
-        id: 1,
-        name: "Kimchi",
-        image: "",
-        rating: 5,
-    },
-    {
-        id: 2,
-        name: "Samgyeopsal",
-        image: "",
-        rating: 5,
-    },
-    {
-        id: 3,
-        name: "Bibimbap",
-        image: "",
-        rating: 2,
-    },
-    {
-        id: 4,
-        name: "Doncasu",
-        image: "",
-        rating: 2,
-    },
-    {
-        id: 5,
-        name: "Kimbap",
-        image: "",
-        rating: 3,
-    },
-];
-
-// function renderFood(dish)
-// {
-//     return <Food name={dish.name} />;
-// }
-
-function App() {
-    // console.log(foodLike.map(renderFood));
-    return (
-        <div>
-            {foodLike.map(dish => (
-                <Food key={dish.id} name={dish.name} rating={dish.rating}/>
-            ))}
-            {/* {foodLike.map(renderFood)} */}
-            {/* <h1>Hello</h1> */}
-            {/* <Food fav="kimchi" somthing={true} papapapa={['hello', 1, 2, 3, 4, true]} /> */}
-            {/* <Food fav="kimchi" />
-            <Food fav="remon" />
-            <Food fav="samgiopsal" />
-            <Food fav="kimchi" /> */}
-        </div>
-    );
-}
-
-Food.propType = {
-    name:PropTypes.string.isRequired,
-    name:PropTypes.number.isRequired,
-};
 export default App;
