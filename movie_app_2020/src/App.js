0ecab678f86ec244de8b2cfdb62414cc6b2aed29
EllieHan93 <hjhhahahoho@naver.com>
2020-08-02 21:13:00 +0900

    노마드 영화 평점웹서비스 실습 완료

--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
+// import Home from './routes/Home';
+// import axios from 'axios';
+// import Movie from './components/Movie';
+import './App.css';
+import { HashRouter, Route } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
+import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
+
+
+function App() {
+    return (
+        <HashRouter>
+            <Navigation/>
+            <Route path="/" exact={true} component={Home}>
+            </Route>
+            <Route path="/about" component={About}>
+            </Route>
+            <Route path="/movie-detail" component={Detail}>
+            </Route>
+        </HashRouter>
+    );
+    // return <Home/>;
+}
 
 //클래스 형 컴포넌트가 되려면, 리액트가 제공하는 컴포넌트 클래스를 반드시 상속받아야 한다. 
-class App extends React.Component {
-    state={
-        isloading:true,
-        Movies:[],
-    };
-    componentDidMount(){
-        setTimeout(()=>{
-            this.setState({isloading: false});
-        }, 6000);
-    }
-    render() {
-        const {isloading} = this.state;
-        return (
-        <div>{isloading ? 'loading...' : 'We are ready'}</div>
-        )
-    }
-}
+// class App extends React.Component {
+//     
+// }
 export default App;
